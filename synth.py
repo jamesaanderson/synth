39f6cd9c9ff0f55c0c7ffcfe359dda3ae90bbcef
James Anderson <me@jamesaanderson.com>
2018-06-03 09:30:34 -0700

    fm & lfo

--- synth.py
+++ synth.py
@@ -41,6 +41,14 @@ def envelope(t, start, final, rate):
    dur = t[t.size-1]
    return (final - start) * np.power(t/dur,rate) + start
 
+# Low Frequency Oscillator
+def lfo(amp_osc, amp_lfo, f_osc, f_lfo, t, phase_osc=0, phase_lfo=0):
+    return (amp_osc + sin(amp_lfo, f_lfo, t, phase_lfo)) * sin(1, f_osc, t, phase_osc)
+
+# Frequency Modulation Synthesis
+def fm(amp_mod, amp_carr, f_mod, f_carr, t, phase_mod=0, phase_carr=0):
+    return amp_carr * np.sin((2 * np.pi * f_carr + sin(amp_mod, f_mod, t, phase_mod)) * t + phase_carr)
+
 def write(audio, sps, out):
     wavfile.write(out, sps, audio) 
 
@@ -64,7 +72,11 @@ DURATION_S = 60
 samples = np.arange(DURATION_S * SPS) / SPS
 amp = envelope(samples,0,0.3,2)
 wave = sin(amp,261.63,samples)
+osc = lfo(0.3,0.3,261.63,samples,20)
 
-write(wave,SPS,'test.wav')
+write(fm(0.3,0.3,13.0815,261.63,samples),SPS,'test.wav')
 # write(triangle(0.3,261.63,samples),SPS,'test.wav')
 # play(wave,SPS)
+
+# plt.plot(samples,fm(0.3,0.3,5,100,samples))
+# plt.show()
